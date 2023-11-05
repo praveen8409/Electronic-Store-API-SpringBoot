a8e01fc42ab76b04bfcfb2a57ea3613acecfa82b
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-11-05 13:00:23 +0530

    Handle MethodArgumentNotValidException

--- src/main/java/com/lcwd/electronic/store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lcwd/electronic/store/exceptions/GlobalExceptionHandler.java
@@ -5,8 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +27,21 @@ public class GlobalExceptionHandler {
         logger.info("Resource not found exception Invoked");
         ApiResponseMessage response = ApiResponseMessage.builder().message(ex.getMessage()).success(true).status(HttpStatus.NOT_FOUND).build();
         return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    }
+
+    // Method not found exception Invoked
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotFoundException(MethodArgumentNotValidException ex){
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        Map<String, Object> response = new HashMap<>();
+        allErrors.stream().forEach(objectError -> {
+            String message = objectError.getDefaultMessage();
+            String field = ((FieldError) objectError).getField();
+            response.put(field, message);
+        });
+
+        logger.info("MethodArgumentNotValidException Invoked ");
+
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
     }
 }
