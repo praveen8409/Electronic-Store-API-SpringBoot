ef47996896bba16eebfdaef8ed78372c2d488267
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-11-08 13:58:47 +0530

    Implement jwt Authentications

--- src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
+++ src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.lcwd.electronic.store.config;
 
+import com.lcwd.electronic.store.security.JwtAuthenticationEntryPoint;
+import com.lcwd.electronic.store.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,12 +17,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
 
 //    @Bean
 //    public UserDetailsService userDetailsService(){
@@ -47,10 +58,18 @@ public class SecurityConfig {
                 .cors()
                 .disable()
                 .authorizeRequests()
+                .antMatchers("/auth/login")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -66,6 +85,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
     }
 
 
--- src/main/java/com/lcwd/electronic/store/controllers/AuthController.java
+++ src/main/java/com/lcwd/electronic/store/controllers/AuthController.java
@@ -1,15 +1,21 @@
 package com.lcwd.electronic.store.controllers;
 
+import com.lcwd.electronic.store.dtos.JwtRequest;
+import com.lcwd.electronic.store.dtos.JwtResponse;
 import com.lcwd.electronic.store.dtos.UserDto;
+import com.lcwd.electronic.store.exceptions.BadApiRequestException;
+import com.lcwd.electronic.store.security.JwtHelper;
+import com.lcwd.electronic.store.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -22,7 +28,40 @@ public class AuthController {
 
     @Autowired
     private ModelMapper mapper;
-    @GetMapping
+    @Autowired
+    private AuthenticationManager manager;
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtHelper helper;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request) {
+        this.doAuthenticate(request.getEmail(), request.getPassword());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        String token = this.helper.generateToken(userDetails);
+        UserDto userDto = mapper.map(userDetails, UserDto.class);
+        JwtResponse response = JwtResponse.builder()
+                .jwtToken(token)
+                .user(userDto).build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private void doAuthenticate(String email, String password) {
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email, password);
+        try {
+            manager.authenticate(authentication);
+        } catch (BadCredentialsException e) {
+            throw new BadApiRequestException(" Invalid Username or Password  !!");
+        }
+
+    }
+
+
+
+    @GetMapping("/current")
     public ResponseEntity<UserDto> getCurrentUser(Principal principal){
         String name = principal.getName();
         return new ResponseEntity<>(mapper.map(userDetailsService.loadUserByUsername(name),UserDto.class), HttpStatus.OK);
